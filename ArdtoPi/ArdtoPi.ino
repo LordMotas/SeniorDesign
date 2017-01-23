435ee148d80a5101054a0406720920c8db026a72
Tanner Olsen <lordmotas@gmail.com>
2017-01-23 11:44:28 -0700

    Updated to begin testing the interrupts on the device

--- ArdtoPi/ArdtoPi.ino
+++ ArdtoPi/ArdtoPi.ino
@@ -1,46 +1,42 @@
-/*void setup() {
-  // put your setup code here, to run once:  
-  //Setup the baud rate to be 9600 to match the Raspberry Pi
+void setup()
+{
   Serial.begin(9600);
-  
-  //Setup interrupts for the Arduino
-  pinMode(13, OUTPUT);     // Pin 13 is output to which an LED is connected
-  digitalWrite(13, LOW);   // Make pin 13 low, switch LED off
-  pinMode(2, INPUT);	   // Pin 2 is input to which a switch is connected = INT0
-  pinMode(3, INPUT);	   // Pin 3 is input to which a switch is connected = INT1
-  attachInterrupt(0, func1, RISING);
-  attachInterrupt(1, func2, RISING);
-}
-*/
-
-const byte ledPin = 13;
-const byte interruptPin = 2;
-volatile byte state = LOW;
-
-void setup() {
-  // put your setup code here, to run once:
-  //Setup interrupts
-  pinMode(ledPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
-  
-  //Setup the baud rate to be 9600 to match the Raspberry Pi
-  Serial.begin(9600);
+  InitialiseIO();
+  InitialiseInterrupt();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(5000);
-  Serial.print("score 100\n");
-  delay(5000);
-  Serial.print("score 1000\n");
-  delay(5000);
-  Serial.print("score -900\n");
-  delay(5000);
-  Serial.print("level 1\n");
-  digitalWrite(ledPin, state);
+  //Interrupts should trigger if hardware happens
+  delay(4000);
+  Serial.print("score 300\n");
+  delay(4000);
+  Serial.print("level \n");
 }
 
-void blink() {
-  state = !state;
+void InitialiseIO(){
+  pinMode(A0, INPUT);	   // Pin A0 is input to which a switch is connected
+  digitalWrite(A0, HIGH);   // Configure internal pull-up resistor
+  pinMode(A1, INPUT);	   // Pin A1 is input to which a switch is connected
+  digitalWrite(A1, HIGH);   // Configure internal pull-up resistor
+  pinMode(A2, INPUT);	   // Pin A2 is input to which a switch is connected
+  digitalWrite(A2, HIGH);   // Configure internal pull-up resistor
+}
+
+void InitialiseInterrupt(){
+  cli();		// switch interrupts off while messing with their settings  
+  PCICR =0x02;          // Enable PCINT1 interrupt
+  PCMSK1 = 0b00000111;
+  sei();		// turn interrupts back on
+}
+
+ISR(PCINT1_vect) {    
+  // Interrupt service routine. Every single PCINT8..14 (=ADC0..5) change
+  // will generate an interrupt: but this will always be the same interrupt routine
+  //Pin A0 is the one that triggered (it was high)
+  if (digitalRead(A0)==0)  Serial.println("A0");
+  //Pin A1 is the one that triggered
+  if (digitalRead(A1)==0)  Serial.println("A1");
+  //Pin A2 is the one that triggered
+  if (digitalRead(A2)==0)  Serial.println("A2");
 }
